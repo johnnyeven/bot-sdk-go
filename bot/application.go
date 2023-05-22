fc91ff9fc5cdb58472198c1d106ab0ddba6e4cfc
liyiwen <johnnyeven@gmail.com>
2023-05-22 15:52:37 +0800

    init

--- bot/application.go
+++ bot/application.go
@@ -11,9 +11,8 @@ import (
 	"encoding/json"
 	"encoding/pem"
 	"errors"
+	"github.com/sirupsen/logrus"
 	"io"
-	"io/ioutil"
-	"log"
 	"net/http"
 	"net/url"
 	"time"
@@ -28,8 +27,8 @@ type Application struct {
 	Handler            func(rawRequest string) string
 }
 
-// 创建一个HTTP服务
-func (this *Application) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// ServeHTTP 创建一个HTTP服务
+func (a *Application) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	defer r.Body.Close()
 	//心跳请求
@@ -39,39 +38,43 @@ func (this *Application) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	body, err := ioutil.ReadAll(r.Body)
+	body, err := io.ReadAll(r.Body)
 	if err != nil {
-		log.Fatal(err)
+		logrus.Fatalf("[DuerOS][Application] ServeHTTP: request read failed: %s", err.Error())
 		HTTPError(w, "request read failed", "Server Error", http.StatusInternalServerError)
 		return
 	}
 
 	r = r.WithContext(context.WithValue(r.Context(), "requestBody", body))
 
-	if !this.Verify(w, r) {
+	if !a.Verify(w, r) {
 		return
 	}
 
-	ret := this.Handler(string(body))
-	w.Write([]byte(ret))
-}
-
-// 启动HTTP服务
-func (this *Application) Start(host string) {
-	err := http.ListenAndServe(host, this)
-
+	ret := a.Handler(string(body))
+	_, err = w.Write([]byte(ret))
 	if err != nil {
-		log.Fatal(err)
+		logrus.Errorf("[DuerOS][Application] ServeHTTP: response write failed: %s", err.Error())
+		return
 	}
 }
 
-// 验证请求是否合法
-func (this *Application) Verify(w http.ResponseWriter, r *http.Request) bool {
-	if !this.DisableVerifyJson && !verifyJSON(w, r, this.AppId) {
+// Start 启动HTTP服务
+func (a *Application) Start(host string) {
+	err := http.ListenAndServe(host, a)
+
+	if err != nil {
+		logrus.Fatalf("[DuerOS][Application] Start failed: %s", err.Error())
+	}
+}
+
+// Verify 验证请求是否合法
+func (a *Application) Verify(w http.ResponseWriter, r *http.Request) bool {
+	if !a.DisableVerifyJson && !verifyJSON(w, r, a.AppId) {
 		return false
 	}
 
-	if !this.DisableCertificate && !validateRequest(w, r) {
+	if !a.DisableCertificate && !validateRequest(w, r) {
 		return false
 	}
 	return true
@@ -79,7 +82,7 @@ func (this *Application) Verify(w http.ResponseWriter, r *http.Request) bool {
 
 func HTTPError(w http.ResponseWriter, logMsg string, err string, errCode int) {
 	if logMsg != "" {
-		log.Println(logMsg)
+		logrus.Errorf("[DuerOS][Application] HTTPError: %s", logMsg)
 	}
 
 	http.Error(w, err, errCode)
@@ -92,18 +95,25 @@ func verifyJSON(w http.ResponseWriter, r *http.Request, appId string) bool {
 	body := r.Context().Value("requestBody").([]byte)
 
 	if err := json.Unmarshal(body, &req.Common); err != nil {
+		logrus.Errorf("[DuerOS][Application] verifyJSON: %s", err.Error())
 		HTTPError(w, err.Error(), "Bad Request", http.StatusBadRequest)
 		return false
 	}
 
 	// Check the timestamp
 	if !req.VerifyTimestamp() && r.URL.Query().Get("_dev") == "" {
+		logrus.Errorf("[DuerOS][Application] verifyJSON: Request too old to continue (>180s).")
 		HTTPError(w, "Request too old to continue (>180s).", "Bad Request", http.StatusBadRequest)
 		return false
 	}
 
 	// Check the app id
 	if !req.VerifyBotID(appId) {
+		logrus.Errorf(
+			"[DuerOS][Application] verifyJSON: DuerOS BotID mismatch! request botId: %s, app botId: %s",
+			req.GetBotId(),
+			appId,
+		)
 		HTTPError(w, "DuerOS BotID mismatch!", "Bad Request", http.StatusBadRequest)
 		return false
 	}
@@ -135,6 +145,7 @@ func IsValidRequest(w http.ResponseWriter, r *http.Request) bool {
 
 	// Verify certificate URL
 	if !verifyCertURL(certURL) {
+		logrus.Errorf("[DuerOS][Application] IsValidRequest: invalid cert URL: %s", certURL)
 		HTTPError(w, "Invalid cert URL: "+certURL, "Not Authorized", http.StatusUnauthorized)
 		return false
 	}
@@ -142,6 +153,7 @@ func IsValidRequest(w http.ResponseWriter, r *http.Request) bool {
 	// Fetch certificate data
 	certContents, err := readCert(certURL)
 	if err != nil {
+		logrus.Errorf("[DuerOS][Application] IsValidRequest: failed to read cert file: %s", err.Error())
 		HTTPError(w, err.Error(), "Not Authorized", http.StatusUnauthorized)
 		return false
 	}
@@ -149,18 +161,21 @@ func IsValidRequest(w http.ResponseWriter, r *http.Request) bool {
 	// Decode certificate data
 	block, _ := pem.Decode(certContents)
 	if block == nil {
+		logrus.Errorf("[DuerOS][Application] IsValidRequest: failed to parse certificate PEM.")
 		HTTPError(w, "Failed to parse certificate PEM.", "Not Authorized", http.StatusUnauthorized)
 		return false
 	}
 
 	cert, err := x509.ParseCertificate(block.Bytes)
 	if err != nil {
+		logrus.Errorf("[DuerOS][Application] IsValidRequest: failed to parse certificate: %s", err.Error())
 		HTTPError(w, err.Error(), "Not Authorized", http.StatusUnauthorized)
 		return false
 	}
 
 	// Check the certificate date
 	if time.Now().Unix() < cert.NotBefore.Unix() || time.Now().Unix() > cert.NotAfter.Unix() {
+		logrus.Errorf("[DuerOS][Application] IsValidRequest: DuerOS certificate expired.")
 		HTTPError(w, "DuerOS certificate expired.", "Not Authorized", http.StatusUnauthorized)
 		return false
 	}
@@ -174,6 +189,7 @@ func IsValidRequest(w http.ResponseWriter, r *http.Request) bool {
 	}
 
 	if !foundName {
+		logrus.Errorf("[DuerOS][Application] IsValidRequest: DuerOS certificate invalid.")
 		HTTPError(w, "DuerOS certificate invalid.", "Not Authorized", http.StatusUnauthorized)
 		return false
 	}
@@ -187,12 +203,14 @@ func IsValidRequest(w http.ResponseWriter, r *http.Request) bool {
 	hash := sha1.New()
 	_, err = io.Copy(hash, bytes.NewReader(r.Context().Value("requestBody").([]byte)))
 	if err != nil {
+		logrus.Errorf("[DuerOS][Application] IsValidRequest: failed to hash request body: %s", err.Error())
 		HTTPError(w, err.Error(), "Internal Error", http.StatusInternalServerError)
 		return false
 	}
 
 	err = rsa.VerifyPKCS1v15(publicKey.(*rsa.PublicKey), crypto.SHA1, hash.Sum(nil), encryptedSig)
 	if err != nil {
+		logrus.Errorf("[DuerOS][Application] IsValidRequest: signature match failed: %s", err.Error())
 		HTTPError(w, "Signature match failed.", "Not Authorized", http.StatusUnauthorized)
 		return false
 	}
@@ -203,12 +221,12 @@ func IsValidRequest(w http.ResponseWriter, r *http.Request) bool {
 func readCert(certURL string) ([]byte, error) {
 	cert, err := http.Get(certURL)
 	if err != nil {
-		return nil, errors.New("Could not download DuerOS cert file.")
+		return nil, errors.New("could not download DuerOS cert file")
 	}
 	defer cert.Body.Close()
-	certContents, err := ioutil.ReadAll(cert.Body)
+	certContents, err := io.ReadAll(cert.Body)
 	if err != nil {
-		return nil, errors.New("Could not read DuerOS cert file.")
+		return nil, errors.New("could not read DuerOS cert file")
 	}
 
 	return certContents, nil
